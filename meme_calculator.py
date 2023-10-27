52a31b65eb239ba87826d6014c754ccbb8bd6c99
kunalsingh8099 <106829657+kunalsingh8099@users.noreply.github.com>
2023-10-27 17:58:34 +0530

    Add files via upload

---
+++ meme_calculator.py
@@ -0,0 +1,10 @@
+print("i am a simple to use calculator")
+a=int(input("tell me a number "))
+b=int(input("whats you second number you want me to add"))
+ap=(a-b)
+print("the answer is "+str(ap))
+ans=input("was it the right outcome? ")
+if ans =="no":
+    print("Fuck you")
+elif ans=="yes":
+    print("thanks")
